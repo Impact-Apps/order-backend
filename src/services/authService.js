349b259af9aefb6580e5bb811fd41794a773f2f1
Russell Brady <russell.brady29@mail.dcu.ie>
2020-04-02 19:38:53 +0100

    refactoring-auth-service

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,15 +1,9 @@
-const UserModel =  require('../models/User');
+const MODEL_PATH = '../models/'
+const UserModel =  require(MODEL_PATH + 'User');
 
-const signUp = async (user) => {
-    let err, userRecord
-    userRecord = await UserModel.create(user);
-    return userRecord;
-}
+const signUp = async user => await UserModel.create(user);
 
-const login = async (email, password) => {
-    const userRecord = await UserModel.findOne({ email: email, password: password})
-    return userRecord
-}
+const login = async (email, password) => await UserModel.findOne({ email: email, password: password});
 
 module.exports = {
     signUp,
