7d993e50d99f76d52a216e4d27105e0ed6293124
Cathal Hughes <cathal.hughes@oliverwyman.com>
2020-04-18 22:43:58 +0100

    Updating all collections on deletion and creation

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -7,23 +7,27 @@ const {isEmpty} = require('lodash')
 
 router.post("/", async (req, res, next) => {
     const body = req.body
-    const [err, item] = await to(menuService.create(body))
-    if(err) return next(err)
-    return res.json(item)
+    const [updateErr, upsertedItems] = await to(itemService.upsertItems(body.items))
+    if(updateErr) return next(updateErr)
+    const itemIds = Object.values(upsertedItems)
+    const menuToCreate = {
+        restaurantId: body.restaurantId,
+        itemIds,
+    }
+    const [createErr, menu] = await to(menuService.create(menuToCreate))
+    if(createErr) return next(createErr)
+    return res.json(menu)
 })
 
 router.patch("/:restaurantId", async (req, res, next) => {
     const body = req.body
-    // console.log(body)
     const [updateErr, upsertedItems] = await to(itemService.upsertItems(body.items))
     if(updateErr) return next(updateErr)
     const itemIds = Object.values(upsertedItems)
-    console.log(itemIds)
-    if(!isEmpty(itemIds)) {
-        // New items were added, add their IDs to the menu
-        const [err] = await to(menuService.updateMenuWithNewItems(itemIds, body.restaurantId))
-        if(err) return next(err)
-    }
+    // New items were added, add their IDs to the menu
+    const currentItems = body.items.map(item => item._id)
+    const [err] = await to(menuService.updateMenuWithNewItems([...currentItems, ...itemIds], body.restaurantId))
+    if(err) return next(err)
     return res.sendStatus(200)
 })
 
@@ -46,9 +50,20 @@ router.delete("/:id", async (req, res, next) => {
     return res.send(deleteResponse)
 })
 
+router.delete("/", async (req, res, next) => {
+    const {restaurantId} = req.query
+    const filter = {
+        restaurantId
+    }
+    const [err, deleteResponse] = await to(menuService.deleteMenuByFilter(filter))
+    if(err) return next(err)
+    return res.send(deleteResponse)
+})
+
 router.get('/:restaurantId/items', async (req, res, next) => {
     const [err, menu] = await to(menuService.getByRestaurantId(req.params.restaurantId))
     if(err) return next(err);
+    if(!menu || isEmpty(menu.itemIds)) return res.send([])
     const [itemsErr, items] = await to(itemService.findItemsForMenu(menu.itemIds)) 
     if(itemsErr) return next(itemsErr)
     return res.send(items)
